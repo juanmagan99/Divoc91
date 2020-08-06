03461059e4495494dad06bbea3d2a0ab59c06d06
luigi <luigitorresmoreira1@gmail.com>
2020-08-06 13:01:41 +0200

    sdasa

--- Divoc/Assets/Scripts/Bullet.cs
+++ Divoc/Assets/Scripts/Bullet.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject hitEffect;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 5f);
+        if(collision.gameObject.GetComponent<EnemyMovement>())
+        {
+            Destroy(collision.gameObject);
+        }
         Destroy(gameObject);
     }
 }
